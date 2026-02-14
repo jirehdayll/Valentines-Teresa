a6182023d06eb5458b6c044b10016daaab2dab48
jirehdayll <jdbjirehdb@gmail.com>
2026-02-14 09:25:13 +0800

    Delete letter.js

--- letter.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const envelope = document.querySelector('.envelope-wrapper');
-    envelope.addEventListener('click', () => {
-        envelope.classList.toggle('open');
-    });
-});
